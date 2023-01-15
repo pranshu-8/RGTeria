11fccaa74806cffce8ac5d591efbe9a9ae12ffdf
pranshu-8 <pranshutewari8@gmail.com>
2023-01-15 11:30:14 +0530

    testing paytm

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -3,17 +3,30 @@ const Noty =require("noty");
 
  function placeOrder(formObject) {
   axios
-    .post("/orders", formObject)
+    .post("/transaction", formObject)
     .then((res) => {
-      new Noty({
-        type: "success",
-        timeout: 1000,
-        text: res.data.message,
-        progressBar: false,
-      }).show();
-      setTimeout(() => {
-        window.location.href = "/customer/orders";
-      }, 1000);
+
+      
+      axios.post("/orders",res).then((rest)=>{
+        new Noty({
+          type: "success",
+          timeout: 1000,
+          text: rest.data.message,
+          progressBar: false,
+        }).show();
+        setTimeout(() => {
+          window.location.href = "/customer/orders";
+        }, 1000);
+
+      }).catch((err)=>{
+        new Noty({
+          type: "error",
+          timeout: 1000,
+          text: err.res.data.message,
+          progressBar: false,
+        }).show();
+       })
+      
     })
     .catch((err) => {
       new Noty({
